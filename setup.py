8ae683eff0c04e0016e693b8269fff20dea5d4bd
Nicholas Devenish <ndevenish@gmail.com>
2021-10-31 16:17:43 +0000

    Add libtbx.Auto

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+print(find_packages(where="src"))
+
+setup(
+    name="cctbx-project-stubs",
+    author="Nicholas Devenish",
+    version="0.1",
+    packages=[
+        "libtbx-stubs",
+        "scitbx-stubs",
+    ],
+    package_dir={"": "src"},
+)
---
+++ src/libtbx-stubs/__init__.pyi
@@ -0,0 +1,3 @@
+class AutoType: ...
+
+Auto: AutoType
