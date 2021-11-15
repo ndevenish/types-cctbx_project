5c91c368d153f49f4695e7c6350417d8086bf3cd
Nicholas Devenish <ndevenish@gmail.com>
2021-11-15 14:59:31 +0000

    Basic flex structures
    Have attempted to use non-instantiable classes for
    declaring the typing stubs.

---
+++ src/scitbx-stubs/array_family/scitbx_array_family_flex_ext.pyi
@@ -0,0 +1,6 @@
+# In real code this is the root and .flex is the derivation.
+#
+# This seems.. somewhat excessive to reproduce in the type system, and
+# most of the things you would want to read from this are just injected
+# onto the actual class instance anyway.
+from .flex import *
