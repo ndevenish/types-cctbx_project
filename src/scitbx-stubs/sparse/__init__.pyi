199d9014bc37a2d08de7a7f1b5e8208bf976b93f
Nicholas Devenish <ndevenish@gmail.com>
2021-11-15 15:15:33 +0000

    Basic fragments of sparse

---
+++ src/scitbx-stubs/sparse/__init__.pyi
@@ -0,0 +1,42 @@
+from typing import Dict, Generator, List, overload
+
+from scitbx.array_family import flex as flex
+
+class vector:
+    pass
+
+class matrix:
+    @overload
+    def __init__(self, rows: int, columns: int) -> None: ...
+    @overload
+    def __init__(
+        self, rows: int, columns: int, elements_by_columns: List[Dict[int, float]]
+    ) -> None: ...
+    @property
+    def n_cols(self) -> int: ...
+    @property
+    def n_rows(self) -> int: ...
+    def as_dense_matrix(self) -> flex.double: ...
+    def col(self, i: int) -> vector: ...
+    def cols(self) -> Generator[vector, None, None]: ...
+
+# as_dense_matrix
+# assign_block
+# clone
+# col
+# cols
+# compact
+# is_structural_zero
+# is_unit_lower_triangular
+# is_upper_triangular
+# n_cols
+# n_rows
+# non_zeroes
+# permute_rows
+# select_columns
+# self_times_diagonal_times_self_transpose
+# self_times_symmetric_times_self_transpose
+# self_transpose_times_diagonal_times_self
+# self_transpose_times_self
+# self_transpose_times_symmetric_times_self
+# transpose
