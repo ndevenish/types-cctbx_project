0782f9bed344ca389f7025d39a7dfcd74a11757c
Nicholas Devenish <ndevenish@gmail.com>
2021-11-16 18:58:08 +0000

    Fill out remaining dxtbx libtbx usage

---
+++ src/libtbx-stubs/test_utils/__init__.pyi
@@ -0,0 +1,12 @@
+from typing import Any, Type
+
+Default: Type
+
+def approx_equal(
+    a1: Any,
+    a2: Any,
+    eps: float = ...,
+    multiplier: float = ...,
+    out: Type[Default] = ...,
+    prefix: str = ...,
+) -> bool: ...
